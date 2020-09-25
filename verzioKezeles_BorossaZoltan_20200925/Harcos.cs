729c93f056fc491d2aa86ffccdcd406ff2b69f47
Petrik Lajos Szg <info@petrik.hu>
2020-09-25 10:07:26 +0200

    Class létrehozása

---
+++ verzioKezeles_BorossaZoltan_20200925/Harcos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verzioKezeles_BorossaZoltan_20200925
+{
+    class Harcos
+    {
+    }
+}
