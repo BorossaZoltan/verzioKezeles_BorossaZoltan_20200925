a515002dd5ba679daa21934be27716fb604c166f
Petrik Lajos Szg <info@petrik.hu>
2020-09-25 10:00:45 +0200

    Projekt létrehozása

---
+++ verzioKezeles_BorossaZoltan_20200925/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verzioKezeles_BorossaZoltan_20200925
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
