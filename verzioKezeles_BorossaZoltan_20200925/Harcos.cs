97278a6a76a3f1fe8b1fb3ca3d15a7e169d62fb7
Petrik Lajos Szg <info@petrik.hu>
2020-09-25 10:25:47 +0200

    Getter Setterek és változók létrehozása

--- verzioKezeles_BorossaZoltan_20200925/Harcos.cs
+++ verzioKezeles_BorossaZoltan_20200925/Harcos.cs
@@ -8,5 +8,27 @@ namespace verzioKezeles_BorossaZoltan_20200925
 {
     class Harcos
     {
+        private string nev;
+        private int szint;
+        private int tapasztalat;
+        private int eletero;
+        private int alapEletero;
+        private int alapSebzes;
+
+        public Harcos(string nev, int statuszSablon)
+        {
+            this.nev = nev;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Szint { get => szint; set => szint = value; }
+        public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
+        public int Eletero { get => eletero; set => eletero = value; }
+        public int AlapEletero { get => alapEletero;}
+        public int AlapSebzes { get => alapSebzes;}
+        public int Sebzes { get => (alapSebzes + szint); }
+        public int SzintLepeshez { get => (10 + szint * 5); }
+        public int MaxEletero { get => (alapEletero + szint * 3); }
+
     }
 }
