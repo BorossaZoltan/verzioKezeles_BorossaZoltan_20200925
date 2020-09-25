3bc1428272809b7f9ccec9e63d7f8c5fb228e499
Petrik Lajos Szg <info@petrik.hu>
2020-09-25 10:05:53 +0200

    HarcosProjekt-re átnevezés

--- verzioKezeles_BorossaZoltan_20200925/Program.cs
+++ verzioKezeles_BorossaZoltan_20200925/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace verzioKezeles_BorossaZoltan_20200925
+namespace HarcosProjekt
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
